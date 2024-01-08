a3b670bb2ea9ab3a403ddf38faa440816c626a16
Mary Danyliuk <marydanyliuk@gmail.com>
2024-01-08 21:37:14 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="filmoteka_reactApp">
+    <BrowserRouter basename="filmoteka-reactApp">
       <App />
     </BrowserRouter>
   </React.StrictMode>
